eccd43e08b3cbcd1356d1a67cd6dfca7b0c2ba0c
eagle-a <eagle-a@users.noreply.github.com>
2026-04-14 22:25:15 +0800

    eagle-a - 04-2026-周二 22:25:14

--- build_all.bat
+++ build_all.bat
@@ -1,4 +1,7 @@
 @echo Cleaning old files......
+@echo Deleting cache files: main.aux, main.out, main.log, main.toc
+del main.aux main.out main.log main.toc 2>nul
+for %%a in (bak,log,aux,toc,dvi,thm,nav,snm,out,exa,bbl,blg,cut,ilg,ind,lof,fen,toe,lot,ten,loa, run.xml) do (del *.%%a /f/s/q/a:-h)
 del /S *.ind *.ilg *.thm *.toe *.aux *.bbl *.blg *.out *.toc *.lof *.fen *.lot *.ten *.log *.bak *.loa *.glo *.ist *.synctex.gz 2>nul
 if exist main.out del /F /Q main.out 2>nul
 
--- clean.bat
+++ clean.bat
@@ -1,1 +1,3 @@
+@echo Deleting cache files: main.aux, main.out, main.log, main.toc
+del main.aux main.out main.log main.toc 2>nul
 for %%a in (bak,log,aux,toc,dvi,thm,nav,snm,out,exa,bbl,blg,cut,ilg,ind,lof,fen,toe,lot,ten,loa, run.xml) do (del *.%%a /f/s/q/a:-h)
